78ea6a6033487ab7b3d99e9ce29fb8caf23f5d03
Johannes Zimmer <johannes95@t-online.de>
2022-08-09 09:49:52 +0200

    added Querschnitte

--- 3DBeam/run_model.py
+++ 3DBeam/run_model.py
@@ -11,50 +11,54 @@ import source.postprocess as postprocess
 import xlwings as xl
 import numpy as np
 
-section_properties_pkl = os_join(*['input', 'nEck_geometry', 'nEck_Lagen.pkl'])
+cross_section_parameters = ['12Eck.pkl']#, 'Kreisring_d12.pkl']#'8Eck.pkl','10Eck.pkl', 
 
-parameters = model_parameters.parameters['holzturm']
+for cross_section in cross_section_parameters:
+    print ('\n', cross_section)
+    section_properties_pkl = os_join(*['inputs', 'geometry', cross_section])
 
-parameters = utils.load_model_data_from_pkl(section_properties_pkl, parameters)
+    parameters = model_parameters.parameters['holzturm']
 
-beam = BeamModel(parameters, adjust_mass_density = True, optimize_frequencies_init=False , apply_k_geo=False)
+    parameters = utils.add_model_data_from_pkl(section_properties_pkl, parameters, set_I_eff=True)
 
-beam_tuner = Optimizations(beam)
+    beam = BeamModel(parameters, adjust_mass_density_for_total = False, optimize_frequencies_init=False , apply_k_geo=False)
 
-target_freqs = parameters['eigen_freqs_target']
+    # beam_tuner = Optimizations(beam)
 
-# beam_tuner.adjust_sway_z_stiffness_for_target_eigenfreq(target_freqs[0], 
-#                                                         target_mode = 0,
-#                                                         print_to_console=True)
+    # target_freqs = parameters['eigen_freqs_target']
 
-# beam_tuner.adjust_mass_density_for_target_eigenfreq(target_freqs[0], 
-#                                                     target_mode = 0,
-#                                                     print_to_console=True)
+    # beam_tuner.adjust_sway_z_stiffness_for_target_eigenfreq(target_freqs[0], 
+    #                                                         target_mode = 0,
+    #                                                         print_to_console=True)
 
-beam_tuner.adjust_nacelle_mass_for_target_eigenfreq(target_freqs[0], 
-                                                    target_mode = 0,
-                                                    print_to_console=True)
-print ('Frenquncies: ')
-for i in range (3):
-    print (round(beam.eigenfrequencies[i],4))
+    # beam_tuner.adjust_mass_density_for_target_eigenfreq(target_freqs[0], 
+    #                                                     target_mode = 0,
+    #                                                     print_to_console=True)
 
-static_load_vector = utils.generate_nodal_force_file(beam.n_nodes, node_of_load_application=11, force_direction='y', magnitude=100000, domain_size='2D')
+    # beam_tuner.adjust_nacelle_mass_for_target_eigenfreq(target_freqs[0], 
+    #                                                     target_mode = 0,
+    #                                                     print_to_console=True)
+    print ('  Frenquncies: ')
+    for i in range (3):
+        print ('   ', round(beam.eigenfrequencies[i],4))
 
-beam.static_analysis_solve(load_vector_file=static_load_vector)
-#postprocess.plot_static_result(beam, ['y'], unit='mm')
-#postprocess.plot_eigenmodes_3D(beam, beam.eigenfrequencies, beam.eigenmodes, dofs_to_plot=['y','g'])
-print ('finished')
+    static_load_vector = utils.generate_nodal_force_file(beam.n_nodes, node_of_load_application=11, force_direction='y', magnitude=100000, domain_size='2D')
 
-# VERGLEICHE
-excel_file = os_join (*['..','..','RFEM','Ergebnisse', 'Einheitslast_ohne Vorspannung.xlsx'])
-wb = xl.Book(excel_file)
-ws = wb.sheets['LF6 - 4.2 Knoten - Verformungen']
+    beam.static_analysis_solve(load_vector_file=static_load_vector)
+    #postprocess.plot_static_result(beam, ['y'], unit='mm')
+    #postprocess.plot_eigenmodes_3D(beam, beam.eigenfrequencies, beam.eigenmodes, dofs_to_plot=['y','g'])
+    print ('finished')
 
-ux_rfem = utils.read_xl_column(ws, start_cell = 'N8', end_row= 23)
-result_rfem = {'y': np.flip((np.array(ux_rfem)))}
-result_rfem['knoten'] = np.flip((np.array(utils.read_xl_column(ws, start_cell = 'K8', end_row= 23))))
+    # VERGLEICHE
+    excel_file = os_join (*['..','..','RFEM','Ergebnisse', 'Einheitslast_ohne Vorspannung.xlsx'])
+    wb = xl.Book(excel_file)
+    ws = wb.sheets['LF6 - 4.2 Knoten - Verformungen']
 
-postprocess.plot_static_result(beam, ['y'], unit='mm', rfem_result=result_rfem)
+    ux_rfem = utils.read_xl_column(ws, start_cell = 'N8', end_row= 23)
+    result_rfem = {'y': np.flip((np.array(ux_rfem)))}
+    result_rfem['knoten'] = np.flip((np.array(utils.read_xl_column(ws, start_cell = 'K8', end_row= 23))))
+
+    postprocess.plot_static_result(beam, ['y'], unit='mm', rfem_result=result_rfem)
 
 
 
